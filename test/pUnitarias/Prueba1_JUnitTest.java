8f9266ae32906baf51fca3f18611018742a83a1e
usr027077 <usr027077@PGFAG919158823.GFBANRURAL.LOCAL>
2015-02-11 16:31:38 -0600

    Incluye marco de pruebas unitarias.

---
+++ test/pUnitarias/Prueba1_JUnitTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pUnitarias;
+
+import java.util.HashSet;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
+import net.sourceforge.jwebunit.*;
+
+//import static net.sourceforge.jwebunit.*;
+
+/**
+ *
+ * @author usr027077
+ */
+public class Prueba1_JUnitTest {
+     
+    public Prueba1_JUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+      
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+ @Test
+    public void test1() {
+
+    }
+}
